f3116e7a7f2f05764152bad4f51c933ccb6db727
Asianwyz <asiawyz@icloud.com>
2018-06-24 09:21:22 +0800

    89 2

---
+++ week_89/002.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 10:59:40 2018
+
+@author: asiaw
+"""
+
+def qsort(position, speed):
+    """
+    对初始位置进行排序
+    """
+    
+
+def carFleet(target, position, speed):
+    """
+    :type target: int
+    :type position: List[int]
+    :type speed: List[int]
+    :rtype: int
+    """
+    
+    qsort(position,speed)
+    
